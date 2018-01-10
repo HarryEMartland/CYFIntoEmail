e6d886aa4ae371875db35352a9cf15ba3427d688
martlandh <HarryMartland@Rentalcars.com>
2018-01-10 20:13:54 +0000

    added html validation

--- test/test.js
+++ test/test.js
@@ -1,17 +1,26 @@
-var assert = require('assert');
+const assert = require('assert');
 const Handlebars = require('handlebars');
 const fs = require('fs');
+const w3c = require('w3c-validate').createValidator();
+const htmllint = require('htmllint');
+
+const htmlLintConf={
+    "tag-bans":[]
+};
 
 
 describe('Email Templates', function () {
 
     const firstName = "some test first name";
 
-    it('html template should compile and set name', function () {
+    it('html template should compile and set name', function (done) {
         const emailHtmlTemplate = Handlebars.compile(fs.readFileSync('src/email.html', 'utf8'));
         const email = emailHtmlTemplate({firstName});
 
-        assert.ok(contains(email, firstName))
+        assert.ok(contains(email, firstName));
+
+
+        w3c.validate('<!DOCTYPE HTML SYSTEM><html><head><title>test</title></head><body>'+email+'</body></html>', done);
     });
 
     it('txt template should compile and set name', function () {
